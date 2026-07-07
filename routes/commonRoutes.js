f2b0fadf5386ca12aa803d9cabd19dc0a0ef63b9
Kaushik Mahata <kaushikmahata5622gmail.com>
2026-07-07 09:23:57 +0530

    package list 07-07-26

--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -1,8 +1,7 @@
 // Common Pages
 export const getFaqPageUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}service/getFaqPage` 
 
-
-// Blogs
+// Blogs Routes
 export const getAllBlogsUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}service/getAllBlogs` 
 export const getParticularBlogUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}service/getParticularBlog` 
 export const getSearchBlogUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}service/getSearchBlog` 
